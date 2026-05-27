fe4d3b87487b3dfee19188d74125d7a27b9b3221
thxgo <paduthiago02@gmail.com>
2026-05-27 19:36:09 -0300

    feat: show speed in MB/s and improve time formatting

--- download-calcutor.py
+++ download-calcutor.py
@@ -5,6 +5,7 @@ def get_unit():
             return unit
         print("Error: enter MB or GB.")
 
+
 def get_float(prompt):
     while True:
         try:
@@ -16,9 +17,10 @@ def get_float(prompt):
         except ValueError:
             print("Error: invalid input, enter a number.")
 
+
 def download_time(file_size, speed_mbps):
     file_size = file_size * 8 
-    total_sec = file_size / speed_mbps 
+    total_sec = file_size / speed_mbps
 
     hours = int(total_sec // 3600)
     minutes = int((total_sec % 3600) // 60)
@@ -26,8 +28,9 @@ def download_time(file_size, speed_mbps):
 
     return hours, minutes, seconds
 
+
 def main():
-    print("Download Time Calculator")
+    print("Download Time Calculator\n")
     
     unit = get_unit()
     file_size = get_float(f'file size ({unit}): ')
@@ -38,7 +41,9 @@ def main():
     speed_mbps = get_float('internet speed (Mbps): ')
     hours, minutes, seconds = download_time(file_size, speed_mbps)
 
-    print(f'\ndownload time: {hours} hour(s), {minutes} minute(s), {seconds} second(s)')
+    print(f'\ndownload time: {hours}h, {minutes}m, {seconds}s\nspeed: {speed_mbps:.0f} Mbps ({speed_mbps / 8:.2f} MB/s)')
+
 
 if __name__ == "__main__":
     main()
+
